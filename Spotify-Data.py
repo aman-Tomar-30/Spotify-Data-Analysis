fb2af613d9b6c0810a33830b77e3d311ca202e1a
aman-Tomar-30 <aman.23bcon0600@jecrcu.edu.in>
2025-12-23 23:02:57 +0530

    Start the Anaysis

--- Spotify-Data.py
+++ Spotify-Data.py
@@ -1,4 +1,21 @@
-import requests
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import os
+from dotenv import load_dotenv
 
-CLIENT_ID = "e018c805a62d401c9dbacfbd23d62a20"
-CLIENT_SECRET = "175768a7f7d14a77951ffd42ad2394ee"
+load_dotenv()
+
+CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyClientCredentials(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET
+    )
+)
+
+playlist_url = "https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M"
+tracks = sp.playlist_items(playlist_url, limit=100)
+print(tracks)
